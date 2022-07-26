9a66c9c59a37d18f153ecd25dc866e705b93c0fb
John Albright <John@IAD-118678AB.lnh.md.smb.rcn.net>
2022-07-26 17:25:58 -0500

    Processing of the POST requests made to /api/translate was finished in api.js; all functional tests were written; all unit and functional tests passed

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Translator = require('../components/translator.js');
+const americanOnly = require('../components/american-only.js');
+const britishOnly = require('../components/british-only.js')
 
 module.exports = function (app) {
   
@@ -8,30 +10,43 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      const locale = req.body.locale;
-      const text = req.body.text;
+      const locale = req.body.locale || req.query.locale;
+      const text = req.body.text || req.query.text;
+      //console.log(locale, text);
+      //console.log("type of text: ", typeof text);
+
       let targetAmerican;
 
       // Guard statements for incorrect input in the post request 
-      if (!text) {
+      if (text == undefined || locale == undefined) {
+        return res.json({ error: "Required field(s) missing" });
+      }
+      if (typeof text === "string" && text.length === 0) {
         return res.json({ error: "No text to translate" });
       }
-      if (locale !== "british-to-american" || locale !== "american-to-british") {
+      if (locale !== "british-to-american" && locale !== "american-to-british") {
         return res.json({ error: "Invalid value for locale field" });
       }
 
       // Assign a value for targetAmerican 
       // Now that locale must be either "british-to-american" or "american-to-british",
       // the variable targetAmerican will definitely have a value of true or false 
-      if (locale === "british-to-american") targetAmerican = false;
-      if (locale === "american-to-british") targetAmerican = true;
+      if (locale === "british-to-american") targetAmerican = true;
+      if (locale === "american-to-british") targetAmerican = false;
+      //console.log(targetAmerican);
+      //console.log(translator.checkAmerican(translator.parseOutWords(text, americanOnly)));
 
       // Guard statements for if all words are valid in the dialect
-      if ((targetAmerican && translator.checkAmerican(text)) || (!targetAmerican && translator.checkBritish(text))) {
-        return res.json({ translation: "Everything looks good to me" });
+      if (text.match(/^\s$/) || (targetAmerican && translator.checkAmerican(translator.parseOutWords(text, americanOnly))) || (!targetAmerican && translator.checkBritish(translator.parseOutWords(text, britishOnly)))) {
+        return res.json({ translation: "Everything looks good to me!" });
       }
 
-      
+      //console.log(translator.translate(text, targetAmerican));
+
+      return res.json({
+        translation: translator.highlight(translator.translate(text, targetAmerican), targetAmerican),
+        text: text
+      });
 
     });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,15 +10,100 @@ let Translator = require('../components/translator.js');
 suite('Functional Tests', () => {
 
     // #1 Translation with text and locale fields: POST request to /api/translate
+    test("#1 Translation with text and locale fields", function(done) {
+        chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+            text: "Mangoes are my favorite food.",
+            locale: "american-to-british"
+        })
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'Response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be of type json');
+            assert.equal(res.body.translation, "Mangoes are my <span class='highlight'>favourite</span> food.");
+            assert.equal(res.body.text, "Mangoes are my favorite food.");
+            done();
+        });
+    });
 
     // #2 Translation with text and invalid locale field: POST request to /api/translate
+    test("#2 Translation with text and invalid locale fields", function(done) {
+        chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+            text: "Mangoes are my favorite food.",
+            locale: "no-way-Jose"
+        })
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'Response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be of type json');
+            assert.equal(res.body.error, "Invalid value for locale field");
+            done();
+        });
+    });
 
     // #3 Translation with missing text field: POST request to /api/translate
+    test("#3 Translation with missing text field", function(done) {
+        chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+            locale: "american-to-british"
+        })
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'Response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be of type json');
+            assert.equal(res.body.error, "Required field(s) missing");
+            done();
+        });
+    });
 
     // #4 Translation with missing locale field: POST request to /api/translate
+    test("#4 Translation with missing locale field", function(done) {
+        chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+            text: "Mangoes are my favorite food."
+        })
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'Response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be of type json');
+            assert.equal(res.body.error, "Required field(s) missing");
+            done();
+        });
+    });
 
     // #5 Translation with empty text: POST request to /api/translate
-    
+    test("#5 Translation with empty text", function(done) {
+        chai
+        .request(server)
+        .post('/api/translate?locale=british-to-american&text=')
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'Response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be of type json');
+            assert.equal(res.body.error, "No text to translate");
+            done();
+        });
+    });
+
     // #6 Translation with text that needs no translation: POST request to /api/translate
+    test("#6 Translation with text that needs no translation", function(done) {
+        chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+            text: "Mangoes are my favorite food.",
+            locale: "british-to-american"
+        })
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'Response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be of type json');
+            assert.equal(res.body.translation, "Everything looks good to me!");
+            done();
+        });
+    });
 
 });
